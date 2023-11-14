ea4efef087f6f7741ec81cc24e180f899e6d190e
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-14 13:58:46 +0100

    Batuketa eragiketaren commita

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Aukeratu eragiketa bat:");
+        System.out.println("1. batuketa \t 2.Kenketa");
+        System.out.println("3. Biderketa \t 4.Zatiketa");
+
+        int zenb1 = sc.nextInt();
+
+        if (zenb1 < 1 || zenb1 > 4 ) {
+            System.out.println("Sartu duzun aukera ez da egokia.");
+        }
+        else {
+            System.out.println("Sartu lehen zenbakia (hamartarra izan daiteke):");
+            float zenb2 = sc.nextFloat();
+            System.out.println("Sartu bigarren zenbakia (hamartarra izan daiteke):");
+            float zenb3 = sc.nextFloat();
+
+            if (zenb3 == 0 && (zenb1 == 4 || zenb1 == 4)){
+
+                System.out.println("Ariketa hau zin da zati zero egin.");
+
+                System.out.println("Ezin da zati 0 egin.");
+
+
+            }
+            else {
+                double emaitza = 0;
+                switch (zenb1){
+                    case 1:
+                        emaitza = zenb2 + zenb3;
+                        break;
+
+                }
+                System.out.println("Eragiketaren emaitza " + emaitza + " da.");
+            }
+        }
+        sc.close();
+    }
+}
